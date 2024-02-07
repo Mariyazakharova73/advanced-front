2090fd4cd9d927c17ca11c0bf8ab9d37a1681634
Mariya Zakharova <zakharovamaria73@yandex.ru>
2024-02-07 13:02:34 +0400

    fix: baseUrl backend

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,7 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000'
-    : 'https://advanced-front-mc2ry5klo-mariyazakharova73s-projects.vercel.app/';
+    : 'https://advanced-front-qz8cqh83x-mariyazakharova73s-projects.vercel.app';
 
 export const $api = axios.create({
   baseURL: baseURL,
